6e814a32f98a56d759188bd5413b140571d3a177
SchroedHaus <matt.schroeder@pm.me>
2025-06-05 11:03:00 +0100

    changed footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 
 const Footer = () => {
   return (
-    <footer className="shadow-md fixed bottom-0 left-0 w-full z-50 bg-white dark:bg-[#343434]">
+    <footer className="shadow-md fixed bottom-0 left-0 w-full z-50 bg-white dark:bg-[#343434] border-t-[1px] border-[#c0c0c0]">
       <div className="max-w-7xl mx-auto px-4 py-3 flex justify-end items-right">
         <p className="text-sm text-gray-600 dark:text-gray-300 flex gap-1">
           Made by <a href="https://sweetchilliwebdesign.com" className="underline">Sweet Chilli Web Design</a> &copy; 2025
