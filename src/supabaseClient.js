f90ad6154e1409358106cd6287a209f87e2f1c30
SchroedHaus <matt.schroeder@pm.me>
2025-06-05 09:56:51 +0100

    updated supabaseClient.js to remove user role key

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,6 +2,5 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-const supabaseServiceRoleKey = import.meta.VITE_SUPABASE_SERVICE_ROLE_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, supabaseServiceRoleKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
